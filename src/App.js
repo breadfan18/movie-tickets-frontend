6a430cec9b6a84db831c07a19a7ab9256ac195ab
Swaroop Uprety <breadfan18@gmail.com>
2022-05-10 18:19:54 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// IMPORT useState
+import React, { useState } from "react";
+import "./index.css";
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1>Template</h1>
+    </div>
+  );
+}
