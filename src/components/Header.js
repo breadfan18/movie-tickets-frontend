f6d37d6fbe68fe3f443cee5d5620a0a564994f37
Swaroop Uprety <breadfan18@gmail.com>
2022-05-19 17:47:19 -0400

    installed react-router-dom;

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Header = props => {
+    // console.log(props, 'are header props')
+    return (
+        <header>
+            <h1>Header Page</h1>
+        </header>
+    )
+}
+
+export default Header;
