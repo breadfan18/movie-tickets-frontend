e761acfe520d0d5a65062eae7fa21b888debc155
Swaroop Uprety <breadfan18@gmail.com>
2022-05-19 19:19:35 -0400

    got movie posters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,9 @@
 const Movie = props => {
+    const posterUrl = `https://image.tmdb.org/t/p/original/${props.movie.poster_path}`
     return (
         <div>
             <h3>{props.movie.title}</h3>
+            <img src={posterUrl} alt="" style={{height: '150px'}} />
         </div>
     )
 }
